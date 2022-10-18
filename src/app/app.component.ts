1c80a37b764c3f244e4cc49c55c319883a327df7
ValentinLagues <valentin.lagues@gmail.com>
2022-10-19 00:23:25 +0200

    quete angular 03.1 terminée

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'quete_angular';
+  title = 'the little world of Didier Branlini';
+
+  isAdmin: boolean = true;
+
+  buttonText: string = (this.isAdmin) ? "Click to become normal" : "Click to become admin";
+
+  constructor() { }
+
+  toggleIsAdmin(){
+    this.isAdmin = !this.isAdmin;
+    if(this.isAdmin){
+      this.buttonText = "Click to become normal";
+    } else {
+      this.buttonText = "Click to become admin";
+    }
+  }
+
+  ngOnInit(): void {
+  }
+  
 }
